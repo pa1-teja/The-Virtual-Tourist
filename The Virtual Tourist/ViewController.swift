e6468154a84486e6f9f1903edc61011b1aa9c2de
Pavan Teja <konavenkatasaipa.teja_ext@novartis.com>
2023-03-11 13:08:24 +0530

    Initial Commit

---
+++ The Virtual Tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Virtual Tourist
+//
+//  Created by TEJAKO3-Old Mac on 11/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
